a85500860142aa6120dddf668b5e3d3b673d1be2
Leandro Parazito <leandroh@gmail.com>
2015-11-24 16:56:08 -0200

    Add express server

--- scripts/web-server.js
+++ scripts/web-server.js
@@ -1,19 +1,16 @@
-var exports = module.exports = {};
-
 var path = require('path');
 var express = require('express');
 var bodyParser = require('body-parser');
 
-var mockServer = express();
-var testAppDir = path.resolve(__dirname, '../app');
+var app = path.resolve(__dirname, '../client');
 var port = 8080;
+var server = express();
+
+server.use('/', express.static(app));
+server.use(bodyParser.json());
 
 // Mock HTTP verbs here (get, post etc)...
 
-mockServer.use('/', express.static(testAppDir));
-mockServer.use(bodyParser.json());
-mockServer.listen(port, function() {
+server.listen(port, function() {
   console.log('Server running like a BOSS in port ' + port);
 });
-
-exports.mockServer = mockServer;
